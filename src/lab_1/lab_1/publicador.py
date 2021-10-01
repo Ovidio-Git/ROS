e49f8081bfe9f9eeedd92b99b5f3b08446c854fe
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-10-01 16:52:05 -0500

    after VSC

--- src/lab_1/lab_1/publicador.py
+++ src/lab_1/lab_1/publicador.py
@@ -5,14 +5,14 @@ from std_msgs.msg import String
 class publicador(Node): # creamos una clase llamada publicador la cual va a heredar la clase node
 	
     def __init__(self):
-	super().__init__('nodo') #usamos nuestro constructor con el nombre de nuestro nodo en este caso el nodo que creamos se llama "node"   
-	self.publicador_ = self.crear_publicador(String, 'Llantas', 10) # llamamos el metodo
- 	# string para que le de a nuestro mensaje el formato necesario para la transmicion y comunicacion con el nodo, adicional a esto
-	# creamos el topico al que nos subscribiremos al cual llamaremos "Llantas", con una cola de espera de 10
+    super().__init__('nodo') #usamos nuestro constructor con el nombre de nuestro nodo en este caso el nodo que creamos se llama "node"   
+    self.publicador_ = self.crear_publicador(String, 'Llantas', 10) # llamamos el metodo
+    # string para que le de a nuestro mensaje el formato necesario para la transmicion y comunicacion con el nodo, adicional a esto
+    # creamos el topico al que nos subscribiremos al cual llamaremos "Llantas", con una cola de espera de 10
 	
-	#ahora definiremos cada cuanto se llevara a cabo la publicacion
-	tiempo = 1  # la setearemos en 1 segundo
-	self.timer = self.create_timer(tiempo, self.publicacion)
+    #ahora definiremos cada cuanto se llevara a cabo la publicacion
+    tiempo = 1  # la setearemos en 1 segundo
+    self.timer = self.create_timer(tiempo, self.publicacion)
 
     def publicacion(self):
         msg = String() #realizamos la instanciacion de la clase string
