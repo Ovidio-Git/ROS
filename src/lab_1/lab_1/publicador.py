5b0ec0001084038ab8a03e7d070af955753889a0
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-10-01 15:45:52 -0500

    Update publicador.py

--- src/lab_1/lab_1/publicador.py
+++ src/lab_1/lab_1/publicador.py
@@ -1,44 +1,33 @@
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
 
-
-class publicador(Node): # creamos una clase llamada publicador la cual va a heredar
-# la calse node
-
+class publicador(Node): # creamos una clase llamada publicador la cual va a heredar la clase node
+	
     def __init__(self):
-        super().__init__('node') #usamos nuestro contructor  con el nombre de nuestro nodo
-# en este caso el nodo que creamos se llama node   
-        self.publicador_ = self.crer_publicador(String, 'Llantas', 10) # llamamos el metodo
-# string para que le de a nuestro mensaje el formato necesario para la trasmicion y comunicacion con el nodo, adicional a esto
-# creamos el topico al que nos subscribiremos llamado Llantas, con una cola de espera de 10
-        
+        super().__init__('node') #usamos nuestro constructor con el nombre de nuestro nodo en este caso el nodo que creamos se llama "node"   
+        self.publicador_ = self.crear_publicador(String, 'Llantas', 10) # llamamos el metodo
+ 	# string para que le de a nuestro mensaje el formato necesario para la transmicion y comunicacion con el nodo, adicional a esto
+	# creamos el topico al que nos subscribiremos al cual llamaremos "Llantas", con una cola de espera de 10
+	
 	#ahora definiremos cada cuanto se llevara a cabo la publicacion
-	tiempo = 1  # lo setearemos en 1 segundo
+	tiempo = 1  # la setearemos en 1 segundo
         self.timer = self.create_timer(tiempo, self.publicacion)
 
-
     def publicacion(self):
-        msg = String()
-        msg.data = 'RPM: 148'
+        msg = String() #realizamos la instanciacion de la clase string
+        msg.data = 'RPM: 148' # definimos el mensaje que enviaremos, con el valor de las revoluciones
         self.publicador_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Mensaje Publicado: "%s"' % msg.data) #publicamos el mensaje predefinido
 
 
-def main(args=None):
-    rclpy.init(args=args)
-
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+def Run():	
+    rclpy.init(args=args) #inicializamos la libreria rclpy
+    nodo = publicador()   #Creamos el nodo
+    rclpy.spin(nodo)      
+    nodo.destroy_node()   #Realizamos la destrucción de los nodos una vez terminado el programa
     rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    main()
+    Run()
