7eacb1b3e180914f215fd0acc2c9f115e0ccb86d
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-10-01 21:32:05 -0500

    rpm conversor

--- src/lab_1/lab_1/Subscriptor.py
+++ src/lab_1/lab_1/Subscriptor.py
@@ -1,4 +1,5 @@
 import rclpy
+import math 
 from rclpy.node import Node
 from std_msgs.msg import String
 
@@ -15,7 +16,16 @@ class Subscriptor(Node):
         self.subcripcion  # prevent unused variable warning
 
     def Subscribirse(self, msg):
-        self.get_logger().info('Publicacion recibida: "%s"' % msg.data) # mostramos el mensaje recibido
+        ########          Capturamos y convertimos el valor de las RPM     #######
+        pub_msg = str(msg.data)
+        mensaje, rpmstr = pub_msg.split()
+        rpm = int(rpmstr)
+        ########          calculamos la velocidad del vehiculo       ###########
+        r= 10
+        radio = r * 0.01 # Convertimos a metros
+        velocida_angular = rpm * ((2*math.pi)/60) # calculamos el valor de la velocidad angular 
+        velocidad = velocida_angular * radio # calculamos el valor de la velocidad
+        self.get_logger().info('Velocidad del vehiculo: "%s"' % str(velocidad)) # mostramos el mensaje recibido
 
 
 def Run():
