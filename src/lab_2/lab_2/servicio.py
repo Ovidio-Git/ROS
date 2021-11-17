78d172c86abb48588576e082f866ed41ab71b971
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-11-17 16:21:12 -0500

    Update servicio.py

--- src/lab_2/lab_2/servicio.py
+++ src/lab_2/lab_2/servicio.py
@@ -2,26 +2,22 @@ import rclpy
 from rclpy.node import Node
 
 
-class MinimalService(Node):
+class RPMService(Node):
 
     def __init__(self):
-        super().__init__('minimal_service')
+        super().__init__('RPM_service')
         self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
 
     def add_two_ints_callback(self, request, response):
         response.sum = request.a + request.b
         self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
-
         return response
 
 
-def main(args=None):
-    rclpy.init(args=args)
-
-    minimal_service = MinimalService()
-
-    rclpy.spin(minimal_service)
-
+def main():
+    rclpy.init(args=None)
+    RPM_service = RPMService()
+    rclpy.spin(RPM_service)
     rclpy.shutdown()
 
 
