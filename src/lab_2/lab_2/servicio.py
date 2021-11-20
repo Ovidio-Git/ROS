9d0154d55c2635a1787ac1eb1de5990dca6e4ddd
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-11-19 20:09:44 -0500

    Update servicio.py

--- src/lab_2/lab_2/servicio.py
+++ src/lab_2/lab_2/servicio.py
@@ -8,11 +8,13 @@ class RPMService(Node):
     def __init__(self):
         super().__init__('RPM_service')
         self.srv = self.create_service(RPMvel, 'rpm', self.RPM_callback)
-
+    
+        
     def RPM_callback(self, request, response):
         #response.vel = float(request.a) * (0.0104719733)
-        response.vel = 290.0
-        self.get_logger().info('Valor constante RPM: %d' % (response.vel))
+        response.r = float(sys.argv[1])
+        response.vel = float(sys.argv[2])
+        self.get_logger().info('Valor RPM: %d Valor radio:' % (response.vel,response.r))
         return response
 
 
