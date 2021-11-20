25c47af0d68f281a2bd646197a149edd1e38e5cd
Ovidio andrade <73707241+Ovidio-Git@users.noreply.github.com>
2021-11-19 20:15:04 -0500

    Update servicio.py

--- src/lab_2/lab_2/servicio.py
+++ src/lab_2/lab_2/servicio.py
@@ -1,22 +1,19 @@
+import sys
 from own_interfaces.srv import RPMvel
 import rclpy
 from rclpy.node import Node
-
 
 class RPMService(Node):
 
     def __init__(self):
         super().__init__('RPM_service')
         self.srv = self.create_service(RPMvel, 'rpm', self.RPM_callback)
-    
-        
+
     def RPM_callback(self, request, response):
-        #response.vel = float(request.a) * (0.0104719733)
         response.r = float(sys.argv[1])
         response.vel = float(sys.argv[2])
         self.get_logger().info('Valor RPM: %d Valor radio:' % (response.vel,response.r))
         return response
-
 
 def Run():
     rclpy.init()
